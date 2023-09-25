8bb0fa09163a195d073a33036025637f78c02b68
alefemoreira <delimaalefe@gmail.com>
2023-09-25 16:06:30 -0300

    formatação do arquivo

--- src/solution/solution.h
+++ src/solution/solution.h
@@ -4,30 +4,24 @@
 #include <iostream>
 #include <vector>
 
-class Solution
-{
+class Solution {
 private:
-    double cost;
-    int vehicles;    
-    std::vector<std::vector<int>> *sequence; // (k + 1) x (n + 1)
+  double cost;
+  int vehicles;
+  std::vector<std::vector<int>> *sequence; // (k + 1) x (n + 1)
 
 public:
-    Solution(/* args */);
-    ~Solution();
-    static Solution* build();
-    void setCost(double cost);
-    void setVehicles(int vehicles);
-    void setSequence(std::vector<std::vector<int>> *sequence);
-    void resume();
+  Solution(/* args */);
+  ~Solution();
+  static Solution *build();
+  void setCost(double cost);
+  void setVehicles(int vehicles);
+  void setSequence(std::vector<std::vector<int>> *sequence);
+  void resume();
 };
 
-Solution::Solution(/* args */)
-{
-}
+Solution::Solution(/* args */) {}
 
-Solution::~Solution()
-{
-}
-
+Solution::~Solution() {}
 
 #endif
