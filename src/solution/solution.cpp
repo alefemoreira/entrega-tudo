36c588d32a8bd66ce10b570965ae79a761c44a25
Weverton Vitor <wevertonvitor01@gmail.com>
2023-09-30 10:54:42 -0300

    Fix

--- src/solution/solution.cpp
+++ src/solution/solution.cpp
@@ -9,81 +9,50 @@ void Solution::build()
 {
   int n = Reader::instance->getDimension();
   int k = Reader::instance->getMAxVehiclesQuantity();
-
-  vector<vector<int>> g(n, vector<int>(n));
-
-  g = {
-      {0, 4, 9, 3, 20},
-      {4, 0, 2, 5, 18},
-      {9, 2, 0, 7, 21},
-      {3, 5, 7, 0, 1},
-      {20, 18, 21, 1, 0},
-  };
+  int l = Reader::instance->getMinimumDelivery();
 
   int maxVehicles = Reader::instance->getMAxVehiclesQuantity();
   int maxVehicleCapacity = Reader::instance->getCarCapacity();
 
   // Pontos não visitados
   vector<int> not_visited;
-  for (int i = 0; i < n; i++)
+  for (int i = 1; i <= n; i++)
   {
     not_visited.push_back(i);
   }
 
+  for (int i = 0; i < k; i++)
+  {
+    this->sequence[i].push_back(0);
+  }
+
   while (!not_visited.empty())
   {
-    int better_vehicle = 0;
+    int better_sequence = 0;
     int better_point = 0;
-    int better_distance_by_capacity = 0;
-    bool hasAvailibleVehicles = false;
+    int better_cost = 0;
+    int visited = 0;
 
     for (int i = 0; i < not_visited.size(); i++)
     {
-      for (size_t j = 0; j < k; j++) // veiculos
+      int vi = not_visited[i];
+      int demand = Reader::instance->getDemand(vi);
+      for (int j = 0; j < k + 1; j++) // veiculos
       {
-        // Confirmando que ainda há carros disponiveis
-        hasAvailibleVehicles = true;
-
-        int distance_by_capacity;
-        int last_point = sequence[j].size() - 1; // Ultimo ponto de uma rota
-
-        // Verificando se o carro suporta a próxima entrega
-        if (this->capacities[j] + g[last_point][i] < maxVehicleCapacity)
+        double cost = this.getCost(i, j);
+        if (cost < better_cost)
         {
-          if (this->capacities[j] > 0)
-          {
-            distance_by_capacity = g[last_point][i];
-          }
-          else // Na primeira alocação considera apenas a capacidade restante
-          {
-            distance_by_capacity = this->capacities[j];
-          }
-
-          if (distance_by_capacity < better_distance_by_capacity)
-          {
-            better_distance_by_capacity = distance_by_capacity;
-            better_vehicle = j;
-            better_point = i;
-          }
+          better_cost = cost;
+          better_point = vi;
+          better_sequence = j;
         }
       }
     }
 
-    if (hasAvailibleVehicles)
-    {
-      // Atualizando a solução
-      this->sequence[better_vehicle].push_back(not_visited[better_point]);
-      not_visited.erase(not_visited.begin() + better_point);
-    }
-    else
-    {
-      // Adicionando rotas restantes aos terceirizados caso não tenha veiculos disponiveis
-      for (int i = 0; i < not_visited.size() - 1; i++)
-      {
-        this->sequence[sequence.size() - 1].push_back(not_visited[i]);
-        not_visited.erase(not_visited.begin() + i);
-      }
-    }
+    // Atualizando a solução
+    this->sequence[better_sequence].push_back(not_visited[better_sequence]);
+    not_visited.erase(not_visited.begin() + better_point);
+    visited += 1;
   }
 }
 
@@ -106,6 +75,28 @@ void Solution::setVehicles(int vehicles) { this->vehicles = vehicles; }
 void Solution::setSequence(vector<vector<int>> *sequence)
 {
   this->sequence = *sequence;
+}
+
+double Solution::getCost(int vi, int j)
+{
+  if (j == this->vehicles)
+  {
+    return Reader::instance->getOutsourcing(vi);
+  }
+  else
+  {
+    int demand = Reader::instance->getDemand(vi);
+    int last = this->sequence[j].back();
+    int cost = Reader::instance->getDistante(last, vi);
+    int maxDemand = Reader::instance->getCarCapacity();
+
+    if (this->capacities[j] + demand >= maxDemand)
+    {
+      return INFINITY;
+    }
+
+    return cost;
+  }
 }
 
 void Solution::resume()
