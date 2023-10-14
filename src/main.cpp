3cd53cc611199e2797f7c7db370e3208c20952c5
alefemoreira <delimaalefe@gmail.com>
2023-10-14 11:28:09 -0300

    style: formatação da função main

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,19 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-    Reader::create(argc, argv[1]);
-    Reader::instance->read();
-    Solution s;
-    s.build();
-    s.resume();
-    cout << endl;
-    cout << endl;
-    s.vnd();
-    s.resume();
-    
-    return 0;
+int main(int argc, char **argv) {
+  Reader::create(argc, argv[1]);
+  Reader::instance->read();
+  Solution s;
+  s.build();
+  s.resume();
+  cout << endl;
+  cout << endl;
+  s.vnd();
+  s.resume();
+
+  cout << "\nCusto: ";
+  cout << s.calculateCost() << endl;
+
+  return 0;
 }
